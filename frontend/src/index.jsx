21891eabcfcc5848bcb4a09b8f2db70e1548c3cc
Fabio William Conceição <fabiowilliamconceicao@MacBook-Air-de-Fabio.local>
2018-03-24 01:11:03 -0300

    paackage updates

---
+++ frontend/src/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+import App from './main/app';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+
+import promise from 'redux-promise';
+import multi from 'redux-multi';
+import thunk from 'redux-thunk';
+
+import Reducers from './main/reducers';
+import Routes from './main/routes';
+
+const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+const store = applyMiddleware(multi, thunk, promise)(createStore)(Reducers, devTools);
+
+ReactDom.render(
+    <Provider store={store}>
+        <Routes />
+    </Provider>,
+    document.getElementById('app')
+);
